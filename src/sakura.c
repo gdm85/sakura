7e184a1b982eb9afca2ba14a6a74c5d38a7947d9
ikenfin <ikenfin@gmail.com>
2015-11-27 15:11:40 +0300

    Fixed behavior of switch_tab_accelerator and move_tab_accelerator on <ctrl> and <ctrl>+<shift> hotkeys. See bug here https://bugs.launchpad.net/sakura/+bug/1438082

--- src/sakura.c
+++ src/sakura.c
@@ -479,7 +479,23 @@ gboolean sakura_key_press (GtkWidget *widget, GdkEventKey *event, gpointer user_
 	}
 
 	/* switch_tab_accelerator + number pressed / switch_tab_accelerator + (left/right) cursor pressed */
-	if ( (event->state & sakura.switch_tab_accelerator) == sakura.switch_tab_accelerator ) {
+	/*
+		if ( (event->state & sakura.switch_tab_accelerator) == sakura.switch_tab_accelerator ) {
+		BUGFIX (https://bugs.launchpad.net/sakura/+bug/1438082):
+			in cases when user configured accelerators like this one:
+				switch_tab_accelerator=4  for ctrl+next[prev]_tab_key
+				move_tab_accelerator=5  for ctrl+shift+next[prev]_tab_key
+			move never works, because switch will be processed firstly
+	*/
+	if(
+		(event->state & sakura.switch_tab_accelerator) == sakura.switch_tab_accelerator
+		&&
+		(
+			sakura.switch_tab_accelerator > sakura.move_tab_accelerator
+			||
+			(event->state & sakura.move_tab_accelerator) != sakura.move_tab_accelerator
+		)
+	) {
 		if ((keyval>=GDK_KEY_1) && (keyval<=GDK_KEY_9) && (keyval<=GDK_KEY_1-1+npages)
 				&& (keyval!=GDK_KEY_1+gtk_notebook_get_current_page(GTK_NOTEBOOK(sakura.notebook)))) {
 			switch(keyval) {
@@ -514,7 +530,16 @@ gboolean sakura_key_press (GtkWidget *widget, GdkEventKey *event, gpointer user_
 	}
 
 	/* move_tab_accelerator + (left/right) cursor pressed */
-	if ( (event->state & sakura.move_tab_accelerator)==sakura.move_tab_accelerator ) {
+	/* if ( (event->state & sakura.move_tab_accelerator)==sakura.move_tab_accelerator ) { */
+	if(
+		((event->state & sakura.move_tab_accelerator) == sakura.move_tab_accelerator)
+		&&
+		(
+			sakura.move_tab_accelerator > sakura.switch_tab_accelerator
+			||
+			(event->state & sakura.switch_tab_accelerator) != sakura.switch_tab_accelerator
+		)
+	) {
 		if (keyval==sakura.prev_tab_key) {
 			sakura_move_tab(BACKWARDS);
 			return TRUE;
