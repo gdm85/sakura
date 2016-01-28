c45f94bfa24c40acb0f8eadbba9999b5b4ab7f48
David Gómez <david@pleyades.net>
2016-01-28 20:43:36 +0100

    * Fixed bug #1525599 (rezso)

--- src/sakura.c
+++ src/sakura.c
@@ -224,6 +224,7 @@ static struct {
 	guint height;
 	glong columns;
 	glong rows;
+	gint scroll_lines;
 	gint label_count;
 	VteTerminalCursorShape cursor_type;
 	bool first_tab;
@@ -295,6 +296,7 @@ struct terminal {
 
 #define ICON_FILE "terminal-tango.svg"
 #define SCROLL_LINES 4096
+#define DEFAULT_SCROLL_LINES 4096
 #define HTTP_REGEXP "(ftp|http)s?://[-a-zA-Z0-9.?$%&/=_~#.,:;+]*[-a-zA-Z0-9.?$%&/=_~#+]"
 #define DEFAULT_CONFIGFILE "sakura.conf"
 #define DEFAULT_COLUMNS 80
@@ -2013,6 +2015,10 @@ sakura_init()
 	}
 	g_free(cfgtmp);
 
+	if (!g_key_file_has_key(sakura.cfg, cfg_group, "scroll_lines", NULL)) {
+		g_key_file_set_integer(sakura.cfg, cfg_group, "scroll_lines", DEFAULT_SCROLL_LINES);
+	}
+	sakura.scroll_lines = g_key_file_get_integer(sakura.cfg, cfg_group, "scroll_lines", NULL);
 
 	if (!g_key_file_has_key(sakura.cfg, cfg_group, "font", NULL)) {
 		sakura_set_config_string("font", DEFAULT_FONT);
@@ -3005,6 +3011,12 @@ sakura_add_tab()
 	/* Configuration for the newly created terminal */
 	GdkRGBA white={255, 255, 255, 1};
 	vte_terminal_set_color_background_rgba(VTE_TERMINAL (term->vte), &white);
+
+	/* Init vte terminal */
+	vte_terminal_set_scrollback_lines(VTE_TERMINAL(term->vte), sakura.scroll_lines);
+	vte_terminal_match_add_gregex(VTE_TERMINAL(term->vte), sakura.http_regexp, 0);
+	vte_terminal_set_mouse_autohide(VTE_TERMINAL(term->vte), TRUE);
+
 	vte_terminal_set_backspace_binding(VTE_TERMINAL(term->vte), VTE_ERASE_ASCII_DELETE);
 	vte_terminal_set_colors_rgba(VTE_TERMINAL(term->vte), 
 							  &sakura.forecolors[term->colorset],
